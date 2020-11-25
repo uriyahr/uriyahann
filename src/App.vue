2f1f39489e7a94e2db5f809aa6841fb00fe8193c
uriyahr <uriyahruiz1@gmail.com>
2020-11-24 20:16:08 -0800

    Complete NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
   <v-app id="landingPage">
+
+    <v-overlay :value="overlay" opacity="1" color="#020c1b">
+      <v-progress-circular indeterminate size="64"> </v-progress-circular>
+    </v-overlay>
+
     <NavBar></NavBar>
 
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import NavBar from './components/NavBar'
-import HelloWorld from './components/HelloWorld';
+import NavBar from "./components/NavBar";
+import HelloWorld from "./components/HelloWorld";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
-    NavBar
+    NavBar,
   },
   data: () => ({
-    //
+    overlay: false,
   }),
+
+  watch: {
+    overlay(val) {
+      val && setTimeout(() => {
+          this.overlay = true;
+        }, 5000);
+    },
+  },
 };
 </script>
 
 <style scoped>
- #landingPage {
-   background-color: #0a192f;
- }
+#landingPage {
+  background-color: #0a192f;
+}
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,21 +1,48 @@
 <template>
   <v-app-bar app color="transparent" elevation="0" hide-on-scroll>
     <!-- insert icon here -->
-
+    <v-img
+      :src="require('../assets/circled-u.svg')"
+      :class="{ activeButton: path === '/',
+               'ml-auto': $vuetify.breakpoint.smAndDown, 
+               'ml-n16': $vuetify.breakpoint.mdAndUp}"
+      @click="route('/')"
+      contain
+      height="42"
+      width="42"
+      id="nav-logo">
+      
+    </v-img>
     <v-spacer />
 
-    <!-- display mobile menu bar as alt-->
+    <!-- display mobile menu bar alt-->
 
     <div v-for="index in rightNav" :key="index.i">
       <button
-        class="link"
+        class="right-nav"
         :class="{ activeButton: path === index.route }"
-        @click="route(index.route)"
-      >
-        <span class="textSecondary--text" id="accentNum"> {{ index.accentNum }} </span>
-        <span class="textPrimary--text" id="title"> {{ index.title }} </span>
+        @click="route(index.route)">
+          <span class="button-hover">
+            <span id="accentNum"> {{ index.accentNum }} </span>
+            <span id="title"> {{ index.title }} </span>
+          </span>
+          
       </button>
     </div>
+
+    <div>
+      <v-btn
+        small
+        :ripple="false"
+        id="resume-button"
+        :class="{ activeButton: path === '/resume'}"
+        @click="route('/resume')"
+        outlined
+        color="textSecondary"
+      >
+        Resume
+      </v-btn>
+    </div> 
   </v-app-bar>
 </template>
 
@@ -25,10 +52,10 @@ export default {
 
   data: () => ({
     rightNav: [
-      { accentNum: "01.", title: "About", route: '/about' },
-      { accentNum: "02.", title: "Experience", route: '/experience' },
-      { accentNum: "03.", title: "Work", route: '/work'},
-      { accentNum: "04.", title: "Contact", route: '/contact' },
+      { accentNum: "01.", title: "About", route: "/about" },
+      { accentNum: "02.", title: "Experience", route: "/experience" },
+      { accentNum: "03.", title: "Work", route: "/work" },
+      { accentNum: "04.", title: "Contact", route: "/contact" },
     ],
   }),
   methods: {
@@ -41,10 +68,50 @@ export default {
   computed: {
     path() {
       return this.$route.path;
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&display=swap");
+
+#accentNum {
+  margin-right: -5px !important;
+  color: #64ffda;
+}
+
+#nav-logo {
+  margin-left: -400px !important;
+}
+
+.right-nav {
+  border: none !important;
+  outline: none !important;
+  font-family: "Fira Code", monospace;
+  font-size: 13px;
+  font-weight: 400;
+}
+
+span.button-hover:hover span{
+  color: #64ffda !important;
+}
+
+#title {
+  padding-right: 2em;
+  color: #ccd6f6;
+}
+
+.v-btn:not(.v-btn--round).v-size--small[data-v-4295d220] {
+  padding: 18px; 
+  font-size: 11px;
+  font-weight: 400; 
+}
+
+.v-ripple__container:active {
+  opacity: 0 !important;
+}
+
+
+
 </style>
